76d907a8c289b478ad86129d21476ff413856821
Milton Salustiano <109237764+miltonneto10@users.noreply.github.com>
2023-08-05 15:29:56 -0300

    Commit Inicial

---
+++ variavel.java
@@ -0,0 +1,11 @@
+//Conceitos de Variável
+public class variavel {
+	//Declaração das variáveis
+	public static void main (String [] args) {
+		String nome = "Milton";
+		int idade = 21
+		boolean = solteiro = true;
+		//imprime em tela o resultado da variável
+		System.out.println ("Texto Simples");
+	}//fim do método
+}//fim da classe
